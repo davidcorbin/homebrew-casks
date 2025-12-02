69a3fe1b34e9c3f7139048633b3c62f70dd23ddc
David Corbin <david@davcor.co>
2025-12-01 22:29:45 -0600

    Base commit

---
+++ Casks/dcminibrowser.rb
@@ -0,0 +1,24 @@
+cask "DCMiniBrowser" do
+  version "v1.0.1"
+  sha256 "d383c0f2f7e04b4a86aab6320f6074c963f60645800838820ad14bfe76d7f647"
+
+  url "https://github.com/davidcorbin/DCMiniBrowser/releases/download/v#{version}/DCMiniBrowser.zip"
+  name "DCMiniBrowser"
+  desc "macOS menu bar application for displaying web pages in a popover"
+  homepage "https://github.com/davidcorbin/DCMiniBrowser"
+  license "MIT"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :ventura"
+
+  app "DCMiniBrowser.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.davcor.dcminibrowser.plist",
+    "~/Library/Saved Application State/com.davcor.dcminibrowser.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# My Casks
+
+## How do I install these formulae?
+
+`brew install davidcorbin/casks/<formula>`
+
+Or `brew tap davidcorbin/casks` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "davidcorbin/casks"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
